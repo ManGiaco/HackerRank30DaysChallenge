b5e47e5030f21bcb8db4893c018aa630a5fbc32e
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2022-11-05 02:31:54 -0300

    Day 01

---
+++ 30days/src/day01/Solution.java
@@ -0,0 +1,7 @@
+package day01;
+
+public class Solution {
+	public static void main(String[] args) {
+		System.out.println("Hello world! This is me trying the '30 days of code' challenge :)");
+	}
+}
